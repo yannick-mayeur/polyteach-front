01365ccfb89d51f7a1e4f8087efa47dcb9e5f2fa
Théo <theo.ponthieu@hotmail.fr>
2019-11-12 12:05:14 +0100

    Create form connexion

--- src/components/Connexion/ConnexionForm.components.js
+++ src/components/Connexion/ConnexionForm.components.js
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function ConnexionForm(props) {
-    return (
-        <>
-            <div className="content">
-                <div className="courseShowcase">
-                    <div className="login-page">
-                        <div className="row mt-3">
-                            <img src="../../static/images/PolyTeach_Logo_RGB.png" className="logohead" />
-                        </div>
-                        <div className="form">
-                            <form className="login-form">
-                                <input type="text" placeholder="firstname.lastname@umontpellier.fr" />
-                                <input type="password" placeholder="password" />
-                                <button className="loginbutton">login</button>
-                                <p className="message">Not registered? <a href="#">Create an account</a></p>
-                                <button className="umloginbutton mt-5">login using UM2 - CAS</button>
-                            </form>
+// REDUX
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
+
+// Action
+import { login } from '../../store/actions/connexion.action'
+
+class ConnexionForm extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            email: '',
+            password: '',
+        }
+    }
+
+    handleChangeEmail = (event) => {
+        this.setState({ email: event.target.value });
+    }
+    handleChangePassword = (event) => {
+        this.setState({ password: event.target.value });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.login (this.state.email, this.state.password)
+    }
+
+    render() {
+        return (
+            <>
+                <div className="content">
+                    <div className="courseShowcase">
+                        <div className="login-page">
+                            <div className="row mt-3">
+                                <img src="../../static/images/PolyTeach_Logo_RGB.png" className="logohead" />
+                            </div>
+                            <div className="form">
+                                <form className="login-form" onSubmit={this.handleSubmit}>
+                                    <p>{this.props.errMessage}</p>
+                                    <input type="text" placeholder="firstname.lastname@umontpellier.fr" onChange={this.handleChangeEmail} />
+                                    <input type="password" placeholder="password" onChange={this.handleChangePassword} />
+                                    <button className="loginbutton">login</button>
+                                    <p className="message">Not registered? <Link to="/signup"> Create an account</Link></p>
+                                    <button className="umloginbutton mt-5">login using UM2 - CAS</button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    }
 }
+
+const mapStateToProps = state => {
+    return { errMessage: state.login.errConnection};
+};
+
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators({ login }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ConnexionForm);
