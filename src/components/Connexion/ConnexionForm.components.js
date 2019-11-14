9ca4cdb00733e4d9a7d044e7cd988d511e55791e
Théo <theo.ponthieu@hotmail.fr>
2019-11-14 02:16:25 +0100

    Modif version React / React for useState

--- src/components/Connexion/ConnexionForm.components.js
+++ src/components/Connexion/ConnexionForm.components.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useInput } from '../utils/form/useInput';
 
 <<<<<<< HEAD
 export default function ConnexionForm(props) {
+    const { value:email, bind:bindEmail, reset:resetEmail} = useInput('');
+    const { value:password, bind:bindPassword, reset:resetPassword } = useInput('');
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        alert(`Submitting Name ${email} ${password}`)
+    }
     return (
         <>
 <<<<<<< HEAD
@@ -12,9 +20,9 @@ export default function ConnexionForm(props) {
                             <img src="../../static/images/PolyTeach_Logo_RGB.png" className="logohead" />
                         </div>
                         <div className="form">
-                            <form className="login-form">
-                                <input type="text" placeholder="firstname.lastname@umontpellier.fr" />
-                                <input type="password" placeholder="password" />
+                            <form className="login-form" onSubmit={handleSubmit}>
+                                <input type="text" placeholder="firstname.lastname@umontpellier.fr" {...bindEmail} />
+                                <input type="password" placeholder="password" {...bindPassword}/>
                                 <button className="loginbutton">login</button>
                                 <p className="message">Not registered? <a href="#">Create an account</a></p>
                                 <button className="umloginbutton mt-5">login using UM2 - CAS</button>
