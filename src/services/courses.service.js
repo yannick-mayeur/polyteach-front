d004b6967282f00679e3c7c9b2d7482a71d13773
Théo <theo.ponthieu@hotmail.fr>
2019-11-17 22:16:53 +0100

    Change api calll to match the unique client created

--- src/services/courses.service.js
+++ src/services/courses.service.js
@@ -2,15 +2,15 @@ import Api from "./Api";
 
 const coursesService = {
     getAllCourses() {
-        return Api().get(`courses`);
+        return Api.get(`courses`);
     },
 
     createCourse (data) {
-        return Api().post(`/courses`, data);
+        return Api.post(`/courses`, data);
     },
 
     getCoursesByClass() {
-        return Api().get('/courses/getAllByClass');
+        return Api.get('/courses/getAllByClass');
     }
 }
 
