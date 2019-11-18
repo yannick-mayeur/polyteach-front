0392abd2be86280a9fe652a34b8478ee9b97454a
Yannick Mayeur <yannick.mayeur@gmail.com>
2019-11-18 16:15:51 +0100

    fix api

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export default () => axios.create({
+const client = axios.create({
     baseURL: "https://polyteach-back-staging.igpolytech.fr"
     //baseURL: "https://polyteach-back.igpolytech.fr"
     //baseURL: "http://localhost:3000"
