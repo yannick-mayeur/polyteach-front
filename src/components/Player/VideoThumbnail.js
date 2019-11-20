3150998960c27da7014908f703493d04001fb7ac
Yannick Mayeur <yannick.mayeur@gmail.com>
2019-11-20 16:50:22 +0100

    Update VideoThumbnail.js

--- src/components/Player/VideoThumbnail.js
+++ src/components/Player/VideoThumbnail.js
@@ -8,7 +8,7 @@ export default function VideoThumbnail(props) {
                className={"courseShowcase__container--course"} 
                onClick={() => props.selectVideo(props.course)}>
                 <video id="videoPlayer" className="plyr__video-embed" width="200" height= "110" playsInline>
-                <source src={props.course.videoUrl} type="video/ogg"/>
+                <source src={props.course.videoUrl} type="video/mp4"/>
                 </video> 
                <h3 className="courseShowcase__container--course-title">{props.course.title}</h3>
             </div>
