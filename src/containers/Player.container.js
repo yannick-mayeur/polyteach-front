588a15156bdf5c04cd361f117a939bf60addc9fc
Théo <theo.ponthieu@hotmail.fr>
2019-11-21 20:03:11 +0100

    Fix conflits

--- src/containers/Player.container.js
+++ src/containers/Player.container.js
@@ -9,37 +9,20 @@ import { fetchCourseWithVideo } from '../store/actions/courses.action';
 
 class PlayerContainer extends Component {
 
-    constructor(props) {
-        super(props);
-    };
-  
-    componentWillMount = () => {
-        this.props.fetchCourse(this.props.match.params.courseID)
-    };
+  constructor(props) {
+    super(props);
+  };
 
-    render() {
-        return (
-            <div>
-                <div>
-                    {
-                    this.props.course && this.props.course.videos[0] && this.props.course.selectedSubtitles? 
-                    <div>
-                        <VideoSelector course={this.props.course} selectVideo = {this.props.selectVideo}/>
+  componentWillMount = () => {
+    this.props.fetchCourse(this.props.match.params.courseID)
+  };
 
-                        <VideoPlayer video={this.props.course.videos.find(video => video.id == this.props.course.selectedCourse)} subtitles={this.props.course.selectedSubtitles} />
-                    </div>
-                    :
-                    <div className="content">
-                    <div className="courseShowcase ml-5">
-                         <h1 style={{textAlign: "center"}}>This course is empty !</h1>
-                    </div>
-                    </div>
-                    }
-                </div>
-            </div>
-        );
+  rateVideo = (video, rate) => {
+    if (video.rating) {
+      this.props.updateRateVideo(video, rate);
+    } else {
+      this.props.rateVideo(video, rate);
     }
-
   }
 
   render() {
@@ -49,7 +32,7 @@ class PlayerContainer extends Component {
           {
             this.props.course && this.props.course.videos[0] && this.props.course.selectedSubtitles ?
               <div>
-                
+
                 <VideoSelector course={this.props.course} selectVideo={this.props.selectVideo} />
                 <VideoPlayer rateVideo={this.rateVideo} video={this.props.course.videos.find(video => video.id == this.props.course.selectedCourse)} subtitles={this.props.course.selectedSubtitles} />
               </div>
