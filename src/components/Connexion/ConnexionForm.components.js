9f19f597fdd13f38b8321063b372211ebc7c83c3
Mellah <ilias.mellah@gmail.com>
2019-11-21 12:32:11 +0100

    oauth

--- src/components/Connexion/ConnexionForm.components.js
+++ src/components/Connexion/ConnexionForm.components.js
@@ -1,74 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-// REDUX
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { Link } from 'react-router-dom';
 
-// ROUTER
-import { withRouter } from "react-router-dom";
+export default function ConnexionForm () {
 
-// Action
-import { login } from '../../store/actions/connexion.action'
+    let state = "state"
+    localStorage.setItem("state", state)
+    const clientId = "c92bcd96-70e3-480a-8bae-c4d7465e4979"
+    const redirectUri = encodeURI('http://localhost:8081/oauth')
+    const uri = 'https://oauth.igpolytech.fr/authorize?client_id='+clientId+'&redirect_uri='+redirectUri+'&state='+state
 
-class ConnexionForm extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            email: '',
-            password: '',
-        }
-    }
-
-    handleChangeEmail = (event) => {
-        this.setState({ email: event.target.value });
-    }
-    handleChangePassword = (event) => {
-        this.setState({ password: event.target.value });
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        this.props.login(this.state.email, this.state.password).then(() => {
-            this.props.history.push("/");
-        })
-
-    }
-
-    render() {
-        return (
-            <>
-                <div className="content">
-                    <div className="courseShowcase">
-                        <div className="login-page">
-                            <div className="row mt-3">
-                                <img src="../../static/images/PolyTeach_Logo_RGB.png" className="logohead" />
-                            </div>
-                            <div className="form">
-                                <form className="login-form" onSubmit={this.handleSubmit}>
-                                    <p className="errorlogin mb-2">{this.props.errMessage}</p>
-                                    <input type="text" placeholder="firstname.lastname@umontpellier.fr" onChange={this.handleChangeEmail} />
-                                    <input type="password" placeholder="password" onChange={this.handleChangePassword} />
-                                    <button className="loginbutton">login</button>
-                                    <p className="message">Not registered? <Link to="/signup"> Create an account</Link></p>
-                                    <button className="umloginbutton mt-5">login using UM2 - CAS</button>
-                                </form>
-                            </div>
+    return (
+        <>
+            <div className="content">
+                <div className="courseShowcase">
+                    <div className="login-page">
+                        <div className="row mt-3">
+                            <img src="../../static/images/PolyTeach_Logo_RGB.png" className="logohead" />
+                        </div>
+                        <div className="form">
+                            <a href={uri} style={{textDecoration:'none', color: 'white'}}>
+                                <button className="umloginbutton mt-5" type="button">Login via Polytech</button>
+                            </a>
                         </div>
                     </div>
                 </div>
-            </>
-        )
-    }
+            </div>
+        </>
+    )
+    
 }
-
-const mapStateToProps = state => {
-    return { errMessage: state.login.errConnection };
-};
-
-const mapDispatchToProps = dispatch => {
-    return bindActionCreators({ login }, dispatch)
-}
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ConnexionForm));
---
+++ src/components/WaitToken.component.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+
+// REDUX
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// action
+import { setUser } from '../store/actions/connexion.action';
+
+import S from '../services'
+
+class WaitToken extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            redirect: false,
+            path: null
+        }
+    }
+
+    componentDidMount() {
+        const code = this.getQueryVariable('code')
+        const stateURI = this.getQueryVariable('state')
+
+        const self = this
+
+        if(code && stateURI) {
+            if (stateURI == localStorage.getItem(stateURI)) {
+                S.connexion.askToken(code)
+                .then(function (response) {
+                    const accessToken = response.data.access_token
+                    const refreshToken = response.data.refresh_token
+                    localStorage.setItem("access_token", accessToken)
+                    localStorage.setItem("refresh_token", refreshToken)
+                    
+                    // check if user is already signed up     
+                    S.connexion.signup(accessToken)
+                    .then(function (response) {
+                        const user = response.data
+                        self.props.setUser(user)
+                        self.setState({
+                            redirect: true,
+                            path: '/dashboard'
+                        })
+                    })
+                })
+                .catch(function (err) {
+                    self.setState({
+                        redirect: true,
+                        path: '/connexion'
+                    })
+                })
+            }
+        } else {
+            self.setState({
+                redirect: true,
+                path: '/connexion'
+            })
+        }
+    }
+
+    getQueryVariable(variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split('&');
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split('=');
+            if (decodeURIComponent(pair[0]) == variable) {
+                return decodeURIComponent(pair[1]);
+            }
+        }
+        return false
+    }
+
+    render() {
+        if (this.state.redirect) {
+            return (<Redirect to={this.state.path}></Redirect>)
+        }
+        return (
+            <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div> 
+            )
+    }
+   
+}
+
+const mapStateToProps = state => {
+    return { };
+};
+
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators({ setUser }, dispatch)
+}
+export default connect(mapStateToProps, mapDispatchToProps)(WaitToken);
