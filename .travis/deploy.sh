69ff3489e2f280469134a312ad2cda8f3434973a
Yannick Mayeur <yannick.mayeur@gmail.com>
2019-11-22 08:22:41 +0100

    Add ENV for urls

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,12 +6,18 @@ ssh-add .travis/travis
 ssh-keyscan igpolytech.fr >> ~/.ssh/known_hosts
 if [ "$1" = "production" ]; then
         git remote add deploy dokku@igpolytech.fr:polyteach
+        mv Dockerfile-production Dockerfile
+        git add Dockerfile
+        git commit -m "Add Dockerfile"
         git config --global push.default simple
-        git push deploy master --force
+        git push deploy HEAD:master --force
 elif [ "$1" = "staging" ]; then
         git remote add deploy dokku@igpolytech.fr:polyteach-staging
+        mv Dockerfile-staging Dockerfile
+        git add Dockerfile
+        git commit -m "Add Dockerfile"
         git config --global push.default simple
-        git push deploy dev:master --force
+        git push deploy HEAD:master --force
 else 
         echo "Bad environment"
 fi
--- webpack.config.js
+++ webpack.config.js
@@ -2,77 +2,81 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
+module.exports = (env) => {
 
-module.exports = {
-
-   entry: "./src/index.js",
-   output: {
+  return {
+    entry: "./src/index.js",
+    output: {
       filename: "bundle.js",
       publicPath: '/',
-   },
-   devServer: {
+    },
+    devServer: {
       historyApiFallback: true,
     },
-   module: {
+    module: {
       rules: [
-         {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-               loader: "babel-loader"
-            }
-         },
-         {
-            test: /\.svg$/,
-            exclude: /node_modules/,
-            use: {
-               loader: 'svg-react-loader'
-            }
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.svg$/,
+          exclude: /node_modules/,
+          use: { loader: 'svg-react-loader' }
 
-         },
-         {
-            test: /\.scss$/,
-            use: [
-               {
-                  loader: MiniCssExtractPlugin.loader,
-                  options: {
-                     publicPath: '../'
-                  }
-               },
-               'css-loader',
-               'sass-loader'
-            ]
-         },
-         {
-            test: /\.(gif|png|jpe?g)$/i,
-            use: [
-               'file-loader',
-               {
-                  loader: 'image-webpack-loader',
-                  options: {
-                     bypassOnDebug: true, // webpack@1.x
-                     disable: true, // webpack@2.x and newer
-                  },
-               },
-            ],
-         }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: '../'
+              }
+            },
+            'css-loader',
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.(gif|png|jpe?g)$/i,
+          use: [
+            'file-loader',
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                bypassOnDebug: true, // webpack@1.x
+                disable: true, // webpack@2.x and newer
+              },
+            },
+          ],
+        }
       ]
-   },
-   node: {
+    },
+    node: {
       fs: "empty"
-   },
-   plugins: [
+    },
+    plugins: [
       new HtmlWebPackPlugin({
-         template: "./src/index.html",
-         filename: "./index.html"
+        template: "./src/index.html",
+        filename: "./index.html"
       }),
       new CopyWebpackPlugin([{ from: 'src/static/images', to: 'static/images' }]),
       new MiniCssExtractPlugin({
-         // Options similar to the same options in webpackOptions.output
-         // both options are optional
-         filename: "main.css"
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: "main.css"
       }),
       new CleanWebpackPlugin(['dist']),
-   ]
-}; 
+      new webpack.DefinePlugin({
+        'process.env.API_URL': JSON.stringify(env.API_URL),
+        'process.env.REDIRECT_URI': JSON.stringify(env.REDIRECT_URI)
+      })
+    ]
+  } 
+};
