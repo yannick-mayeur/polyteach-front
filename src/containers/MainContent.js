d506e8ef59e51d68fae6e14c967415ee4058da65
Théo <theo.ponthieu@hotmail.fr>
2019-11-22 22:18:08 +0100

    conflits

--- src/containers/MainContent.js
+++ src/containers/MainContent.js
@@ -5,8 +5,8 @@ import { connect } from 'react-redux';
 
 // Components
 import { ScrollCourses } from './ScrollCourses.container';
-import {LiveCard} from '../components/LiveCard';
-const studentLive_URL= 'livestudent/';
+import { LiveCard } from '../components/LiveCard';
+const studentLive_URL = 'livestudent/';
 
 // Store
 import { addCourseToEdit, fetchAllMyCourses, removeCourse, bookmarkCourse, unbookmarkCourse, rateCourseAndRefresh, updateRateCourseAndRefresh } from '../store/actions/courses.action';
@@ -60,49 +60,36 @@ class MainContent extends Component {
 
 
   render() {
-<<<<<<< HEAD
-    return (
-      <div className="content">
-        <div className="courseShowcase ml-5">
-                {this.props.courses && this.props.courses.length > 0 && this.props.courses.map(course => course.bookmarked).reduce((acc, current) => acc || current) ?
-                    <ScrollCourses user={this.props.user} removeCourse={this.props.removeCourse} rateCourse={this.rateCourse} toogleBookmarkCourse={this.toogleBookmarkCourse} courses={this.props.courses.filter(course => course.bookmarked)} name="My Bookmarked Courses"></ScrollCourses>
-                    : <h1 style={{ textAlign: "center" }} className="mt-5">{this.props.user && this.props.user.role === 0? "You have no bookmarked courses yet :(" : ""}</h1>}
-
-        {this.props.courses && this.props.courses.length > 0 ?
-          <ScrollCourses user={this.props.user} removeCourse={this.props.removeCourse} rateCourse={this.rateCourse} toogleBookmarkCourse={this.toogleBookmarkCourse} courses={this.props.courses.filter(course => course.name.toLowerCase().includes(this.props.search.toLowerCase()))} name="My Courses"></ScrollCourses>
-          : <h1 style={{ textAlign: "center" }} className="mt-5">You have no courses yet :(</h1>}
-
-
-          <div className="container">
-            <div className="card-group">
-              <div className="row">
-                {(this.props.lives && this.props.lives.length > 0) ?
-                    this.props.lives.map(live => {
-                      const live_URL = ''+studentLive_URL+ live.idsession;
-                      return <LiveCard key={live.idsession} live={live} liveURL={live_URL} />
-                    })
-                    :
-                    <div className="mx-auto text-center"> <h1>There are no teachers currently livestreaming. </h1> </div>
-                }
-              </div>
-            </div>
-=======
     if (this.state.redirect) {
       return <Redirect to='/courseEditor' />
-    } else {
+    }
+    else {
       return (
         <div className="content">
-
           <div className="courseShowcase ml-5">
             {this.props.courses && this.props.courses.length > 0 && this.props.courses.map(course => course.bookmarked).reduce((acc, current) => acc || current) ?
               <ScrollCourses user={this.props.user} removeCourse={this.props.removeCourse} rateCourse={this.rateCourse} toogleBookmarkCourse={this.toogleBookmarkCourse} courses={this.props.courses.filter(course => course.bookmarked)} name="My Bookmarked Courses"></ScrollCourses>
               : <h1 style={{ textAlign: "center" }} className="mt-5">{this.props.user && this.props.user.role === 0 ? "You have no bookmarked courses yet :(" : ""}</h1>}
 
             {this.props.courses && this.props.courses.length > 0 ?
-              <ScrollCourses editCourse={this.editCourse} user={this.props.user} removeCourse={this.props.removeCourse} rateCourse={this.rateCourse} toogleBookmarkCourse={this.toogleBookmarkCourse} courses={this.props.courses.filter(course => course.name.toLowerCase().includes(this.props.search.toLowerCase()))} name="My Courses"></ScrollCourses>
+              <ScrollCourses user={this.props.user} removeCourse={this.props.removeCourse} rateCourse={this.rateCourse} toogleBookmarkCourse={this.toogleBookmarkCourse} courses={this.props.courses.filter(course => course.name.toLowerCase().includes(this.props.search.toLowerCase()))} name="My Courses"></ScrollCourses>
               : <h1 style={{ textAlign: "center" }} className="mt-5">You have no courses yet :(</h1>}
 
->>>>>>> editCourse
+
+            <div className="container">
+              <div className="card-group">
+                <div className="row">
+                  {(this.props.lives && this.props.lives.length > 0) ?
+                    this.props.lives.map(live => {
+                      const live_URL = '' + studentLive_URL + live.idsession;
+                      return <LiveCard key={live.idsession} live={live} liveURL={live_URL} />
+                    })
+                    :
+                    <div className="mx-auto text-center"> <h1>There are no teachers currently livestreaming. </h1> </div>
+                  }
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       );
@@ -111,26 +98,17 @@ class MainContent extends Component {
 }
 
 const mapStateToProps = (state) => {
-<<<<<<< HEAD
-  return { courses: state.courses.myCourses,
-            search: state.search.searchQueryCourse,
-            user: state.login.user,
-            lives: state.courses.myLives,}
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({ fetchAllMyCourses, bookmarkCourse, getActiveLives, unbookmarkCourse, rateCourseAndRefresh, updateRateCourseAndRefresh, removeCourse }, dispatch)
-=======
   return {
     courses: state.courses.myCourses,
     search: state.search.searchQueryCourse,
-    user: state.login.user
+    user: state.login.user,
+    lives: state.courses.myLives,
   }
 }
 
+
 const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({ addCourseToEdit, fetchAllMyCourses, bookmarkCourse, unbookmarkCourse, rateCourseAndRefresh, updateRateCourseAndRefresh, removeCourse }, dispatch)
->>>>>>> editCourse
+  return bindActionCreators({getActiveLives,  addCourseToEdit, fetchAllMyCourses, bookmarkCourse, unbookmarkCourse, rateCourseAndRefresh, updateRateCourseAndRefresh, removeCourse }, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(MainContent);
